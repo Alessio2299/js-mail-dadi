1b1a23584070dbd8c74ddeb4bdde42e4dfbf47f3
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-25 18:08:00 +0100

    End Exercise Gioco dei Dadi

--- Gioco dei dadi/main.js
+++ Gioco dei dadi/main.js
@@ -2,14 +2,63 @@ let randomNumberPlayer = Math.floor(Math.random()* 6 + 1);
 let randomNumberComputer = Math.floor(Math.random()* 6 + 1);
 let myDice = document.getElementById("mydice");
 let computerDice = document.getElementById("computer-dice");
-let button = document.querySelector(".btn");
+let start = document.querySelector(".start");
+let reset = document.querySelector(".reset")
 let container = document.getElementById("game");
-console.log(computerDice);
-console.log(myDice);
+let result = document.getElementById("result");
+ console.log(result)
 
-button.addEventListener("click",
+start.addEventListener("click",
   function(){
     container.classList.remove("d-none");
+    
+    if(randomNumberPlayer==2){
+      myDice.classList.remove("dice1");
+      myDice.classList.add("dice2");
+    } else if(randomNumberPlayer==3){
+      myDice.classList.remove("dice2");
+      myDice.classList.add("dice3");
+    } else if(randomNumberPlayer==4){
+      myDice.classList.remove("dice3");
+      myDice.classList.add("dice4");
+    } else if(randomNumberPlayer==5){
+      myDice.classList.remove("dice4");
+      myDice.classList.add("dice5");
+    } else if(randomNumberPlayer==6){
+      myDice.classList.remove("dice5");
+      myDice.classList.add("dice6");
+    }
+    if(randomNumberComputer==2){
+      computerDice.classList.remove("dice1");
+      computerDice.classList.add("dice2");
+    } else if(randomNumberComputer==3){
+      computerDice.classList.remove("dice2");
+      computerDice.classList.add("dice3");
+    } else if(randomNumberComputer==4){
+      computerDice.classList.remove("dice3");
+      computerDice.classList.add("dice4");
+    } else if(randomNumberComputer==5){
+      computerDice.classList.remove("dice4");
+      computerDice.classList.add("dice5");
+    } else if(randomNumberComputer==6){
+      computerDice.classList.remove("dice5");
+      computerDice.classList.add("dice6");
+    }
+    
+    if(randomNumberPlayer < randomNumberComputer){
+      result.innerHTML= "Hai perso!";
+      result.classList.add("text-danger");
+    } else if (randomNumberPlayer == randomNumberComputer){
+      result.innerHTML= "Hai pareggiato!";
+      result.classList.add("text-warning");
+    } else{
+      result.innerHTML= "Hai vinto!";
+      result.classList.add("text-success");
+    } 
   }
 )
-
+reset.addEventListener("click",
+  function(){
+    location.reload();
+  }
+)
