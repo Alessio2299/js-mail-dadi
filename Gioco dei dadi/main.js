06165dd6daa5da1f5a6e3868daa2d785655b0fb3
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-25 17:22:04 +0100

    Created part HTML

--- Gioco dei dadi/main.js
+++ Gioco dei dadi/main.js
@@ -1,13 +1,15 @@
 let randomNumberPlayer = Math.floor(Math.random()* 6 + 1);
 let randomNumberComputer = Math.floor(Math.random()* 6 + 1);
+let myDice = document.getElementById("mydice");
+let computerDice = document.getElementById("computer-dice");
+let button = document.querySelector(".btn");
+let container = document.getElementById("game");
+console.log(computerDice);
+console.log(myDice);
 
-if(randomNumberPlayer < randomNumberComputer){
-  alert("Il tuo numero é: " + randomNumberPlayer + " mentre il numero del Computer è: " + randomNumberComputer);
-  alert("Hai perso");
-} else if (randomNumberPlayer == randomNumberComputer){
-  alert("Il tuo numero é: " + randomNumberPlayer + " mentre il numero del Computer è: " + randomNumberComputer);
-  alert("Hai pareggiato");
-} else{
-  alert("Il tuo numero é: " + randomNumberPlayer + " mentre il numero del Computer è: " + randomNumberComputer);
-  alert("Hai vinto!");
-}
+button.addEventListener("click",
+  function(){
+    container.classList.remove("d-none");
+  }
+)
+
