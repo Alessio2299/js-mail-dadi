0cfe23507be78ec5e1d9a587740298deac6cdfbc
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-25 19:12:57 +0100

    End Exercise Mail

--- Mail/main.js
+++ Mail/main.js
@@ -1,12 +1,26 @@
-const mail = ["ciccio1234@gmail.com", "pippo298@gmail.com", "mario4332@gmail.com"];
+const listMail = ["ciccio1234@gmail.com", "pippo298@gmail.com", "mario4332@gmail.com"];
+const access = document.getElementById("access");
+let myMail = document.getElementById("email");
 
-let myMail = prompt("Inserisci la tua email");
+let mail;
+access.addEventListener("click",
+  function(){
+    for (i=0; i < listMail.length; i++){
 
-for (i=0; i < mail.length; i++){
-
-  let currentMail = mail[i];
-
-  if(currentMail == myMail){
-    alert("Hai effettuato l'accesso!")
+      let currentMail = listMail[i];
+    
+      if(currentMail == myMail.value){
+        mail = true;
+      }
+    }
+    if(mail == true){
+      alert("Hai effettuato l'accesso");
+    } else{
+      alert("Non puoi effettuare l'accesso.");
+    }
+    myMail.value = "";
   }
-}
+)
+
+
+
